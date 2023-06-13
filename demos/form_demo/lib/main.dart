9ce21fcab61cad4a408c9187126230ab07810e35
Bugfix Jacques <jacques@iconica.nl>
2023-06-13 16:00:05 +0200

    form demo

---
+++ demos/form_demo/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String? email;
+  String? password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 250,
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text('Email'),
+                  ),
+                  onSaved: (String? value) {
+                    email = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Field is required';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    label: Text('Password'),
+                  ),
+                  onSaved: (String? value) {
+                    password = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Field is required';
+                    }
+
+                    if (value.length < 6) {
+                      return 'Field is to short';
+                    }
+
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                FilledButton(
+                  onPressed: () {
+                    var form = formKey.currentState!;
+
+                    if (form.validate()) {
+                      form.save();
+
+                      //Login (email, password)
+                      print(email);
+                      print(password);
+                    }
+                  },
+                  child: const Text('Submit'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
