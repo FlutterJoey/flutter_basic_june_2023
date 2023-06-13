5a08dc2fb645aa8ae63ef6754184e99422652f8b
Joey Boerwinkel <joey@iconica.nl>
2023-06-13 09:52:50 +0200

    add inherited widget example and keys

--- demos/widgets_demo/lib/main.dart
+++ demos/widgets_demo/lib/main.dart
@@ -4,38 +4,148 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final keys = [
+    GlobalKey<_UnstableCounterState>(),
+    GlobalKey<_UnstableCounterState>(),
+  ];
+  var currentKey = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Row(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.green,
-              ),
-            ),
-            const SizedBox(
-              height: 100,
-              width: 100,
-              child: CircularContainer(
-                color: Colors.blueGrey,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.red,
-                alignment: Alignment.center,
-                child: const MyStatefulWidget(
-                  initalCount: 5,
+    Theme.of(context);
+    return MyInheritedStatefulWidget(
+      child: MaterialApp(
+        home: Scaffold(
+          body: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  alignment: Alignment.center,
+                  child: UnstableCounter(
+                    key: keys[currentKey],
+                    onUpdateCount: (count) {
+                      print(count);
+                      if (count > 5) {
+                        setState(() {
+                          currentKey = 1;
+                        });
+                      }
+                      if (count > 10) {
+                        setState(() {
+                          currentKey = 0;
+                        });
+                      }
+                    },
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 100,
+                width: 100,
+                child: CircularContainer(
+                  color: Colors.blueGrey,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                  child: const MyStatefulWidget(
+                    initalCount: 5,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class MyInheritedWidget extends InheritedWidget {
+  const MyInheritedWidget({
+    required this.state,
+    super.key,
+    required super.child,
+  });
+
+  final MyInheritedStatefulWidgetState state;
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return true;
+  }
+}
+
+class MyInheritedStatefulWidget extends StatefulWidget {
+  const MyInheritedStatefulWidget({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  State<MyInheritedStatefulWidget> createState() =>
+      MyInheritedStatefulWidgetState();
+}
+
+class MyInheritedStatefulWidgetState extends State<MyInheritedStatefulWidget> {
+  String name = '';
+
+  void updateName(String name) {
+    print('updating name $name');
+    setState(() {
+      this.name = name;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MyInheritedWidget(state: this, child: widget.child);
+  }
+}
+
+class UnstableCounter extends StatefulWidget {
+  const UnstableCounter({
+    required this.onUpdateCount,
+    super.key,
+  });
+
+  final void Function(int count) onUpdateCount;
+
+  @override
+  State<UnstableCounter> createState() => _UnstableCounterState();
+}
+
+class _UnstableCounterState extends State<UnstableCounter> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: () {
+        setState(() {
+          count++;
+        });
+        widget.onUpdateCount(count);
+
+        var inheritedWidget =
+            context.getInheritedWidgetOfExactType<MyInheritedWidget>();
+        inheritedWidget?.state.updateName('James the $count');
+      },
+      child: Text(
+        '$count',
       ),
     );
   }
@@ -109,9 +219,13 @@ class MyStatefulState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
+    var inheritedWidget =
+        context.getInheritedWidgetOfExactType<MyInheritedWidget>();
+    var name = inheritedWidget?.state.name ?? 'No name';
+
     return FilledButton(
       onPressed: pressButton,
-      child: Text('$count'),
+      child: Text('$count $name'),
     );
   }
 }
